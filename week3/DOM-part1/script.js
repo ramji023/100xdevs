05c94f1d671dae71912bd735f844e66333743207
Ram ji <mishraramji039@gmail.com>
2025-04-12 17:45:11 +0530

    make some  change in FastFingers repo

---
+++ week3/DOM-part1/script.js
@@ -0,0 +1,49 @@
+
+/*
+// fetching elements
+There are 5 popular methods available for fetching DOM elements - 
+1. querySelector
+2. querySelectorAll
+3. getElementById
+4. getElementByClassName
+5. getElementsByTagName
+
+
+// updating elements
+1. .innerHTML
+2. .textContent
+
+
+// adding elements
+1. createElement
+
+
+// deleting elements
+1. remove()
+2. removeChild()
+
+
+
+// document.querySelector('button').addEventListener('click',addTodo)
+
+function addTodo() {
+    const inputElement = document.querySelector('input')
+    console.log(inputElement.value);
+}
+
+const element = document.querySelector('h1')
+// element.innerHTML = "<button>how are you</button>"
+// element.innerText = "<button>how are you</button>"
+// element.style.backgroundColor= 'purple' 
+
+
+
+const counterElement = document.querySelector('h2')
+console.log(counterElement.innerText);
+
+setInterval(()=>{
+  counterElement.innerText = parseInt(counterElement.innerText)+1;
+},1000)
+
+*/
+
