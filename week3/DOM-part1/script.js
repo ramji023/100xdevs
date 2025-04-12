1e769ece76231427c5cb02ddeddaa597f11e9f7b
Ram ji <mishraramji039@gmail.com>
2025-04-12 19:09:21 +0530

    design todo app using DOM property

--- week3/DOM-part1/script.js
+++ week3/DOM-part1/script.js
@@ -47,3 +47,66 @@ setInterval(()=>{
 
 */
 
+// design todo app
+let id = 1;  // first initilize the id
+
+const todosElement = document.querySelector(".todos")
+
+if (id === 1) {
+  todosElement.innerHTML = "<p>There is no todo yet</p>"
+}
+
+
+// whenever user click to add todo button
+function addTodo() {
+  // fetch the input value from input box
+  const inputElement = document.querySelector("input")
+  const addTodo = inputElement.value
+  if (addTodo) {
+    inputElement.value = '';
+    if (id === 1) {
+      todosElement.innerHTML = '';
+    }
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('id', id) // set the id attribute
+
+    const newSpanElement = document.createElement('span')
+    const newDeleteButton = document.createElement('button')
+    newDeleteButton.setAttribute('onClick', `deleteTodo(${id})`);
+    const newEditButton = document.createElement('button')
+    newEditButton.setAttribute('onClick', `editTodo(${id})`);
+
+    newSpanElement.innerText = `${id}. ${addTodo}`;
+    newEditButton.innerText = 'Edit'
+    newDeleteButton.innerText = 'delete'
+
+    newDiv.appendChild(newSpanElement)
+    newDiv.appendChild(newDeleteButton)
+    newDiv.appendChild(newEditButton)
+
+    todosElement.appendChild(newDiv)
+    return id++;
+  } else {
+    alert("enter the todo")
+  }
+}
+
+function deleteTodo(id) {
+  const deletedElement = document.getElementById(String(id));
+  console.log(deletedElement);
+  todosElement.removeChild(deletedElement);
+}
+
+function editTodo(id) {
+  const editTodoElement = document.getElementById(String(id));
+  // console.log(editTodoElement)
+  const editableContent = editTodoElement.children.item(0).textContent.slice(2).trim()
+  console.log(editableContent);
+  const inputElement = document.querySelector("input")
+  inputElement.value = editableContent;
+  todosElement.removeChild(editTodoElement);
+}
+
+
+
+
