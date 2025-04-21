900e3817afd578866b4c4111c2a6492ede06ed1e
Ram ji <mishraramji039@gmail.com>
2025-04-21 23:36:09 +0530

    learn about query Params

---
+++ week5/httpServer_and_middlewares/index.js
@@ -0,0 +1,51 @@
+const express = require("express")
+
+const app = express()
+
+
+app.get('/test', (req, res) => {
+    res.send("here is my test route")
+})
+
+
+// here we will practice params and query parameters
+
+// addition
+app.get("/add", function (req, res) {
+    const { a, b } = req.query;
+    console.log(a)
+    console.log(b)
+    if (a && b) {
+        return res.send(Number(a) + Number(b))
+    }
+})
+//substraction
+app.get("/substraction", function (req, res) {
+    const { a, b } = req.query;
+    console.log(a)
+    console.log(b)
+    if (a && b) {
+        return res.send(Number(a) - Number(b))
+    }
+})
+// multiply
+app.get("/multiplication", function (req, res) {
+    const { a, b } = req.query;
+    console.log(a)
+    console.log(b)
+    if (a && b) {
+        return res.send(Number(a) * Number(b))
+    }
+})
+// divide 
+app.get("/divide", function (req, res) {
+    const { a, b } = req.query;
+    console.log(a)
+    console.log(b)
+    if (a && b) {
+        return res.send(Number(a) / Number(b))
+    }
+})
+
+
+app.listen(3000);
